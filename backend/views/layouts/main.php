6d6bb130837e01121946e6dbc2e022c3a8d65875
xuguoliangjj <1044748759@qq.com>
2015-05-02 22:03:47 +0800

    修改layout

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -25,7 +25,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'YII2 CMS',
+                'brandLabel' => 'Yii CMS',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
