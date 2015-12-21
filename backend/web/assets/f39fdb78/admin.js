debbc2907d60bd60a41c651686931fe1aa1eb279
xuguoliangjj <1044748759@qq.com>
2015-12-21 20:04:11 +0800

    修改

--- backend/web/assets/f39fdb78/admin.js
+++
@@ -1,6 +0,0 @@
-$(document).on('pjax:send', function() {
-    $.fn.editorDridView = layer.load(0, {shade: [0.3,'#fff']});
-})
-$(document).on('pjax:complete', function() {
-    layer.close($.fn.editorDridView);
-})
